93f1963300f7e0e73b8acc3dbe1c222097566e05
Enio Amarantes <37565576+EnioAmarantes@users.noreply.github.com>
2019-09-20 23:48:11 -0300

    Add files via upload

--- Artefatos/Projeto/client/src/app/training/training.component.ts
+++ Artefatos/Projeto/client/src/app/training/training.component.ts
@@ -1,1 +1,18 @@
-teste
+import { Component, OnInit } from '@angular/core';
+
+import { trainings } from '../trainings';
+
+@Component({
+  selector: 'app-training',
+  templateUrl: './training.component.html',
+  styleUrls: ['./training.component.scss']
+})
+export class TrainingComponent implements OnInit {
+
+  trainings = trainings;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
