f97b969019540a5b4f95e6ba26348e458d98ca89
Enio Amarantes <37565576+EnioAmarantes@users.noreply.github.com>
2019-09-20 23:46:22 -0300

    Add files via upload

---
+++ Artefatos/Projeto/client/src/app/trainings.ts
@@ -0,0 +1,23 @@
+export const trainings = [
+    {
+        name: 'Supino',
+        rep: 10,
+        carga: '10Kg',
+        type: 'Peito',
+        description: 'Descrição do exercício'
+},
+    {
+        name: 'Remada Alta',
+        rep: 10,
+        carga: '10Kg',
+        type: 'Costas',
+        description: 'Descrição do exercício'
+    },
+    {
+        name: 'Leg Press',
+        rep: 10,
+        carga: '10Kg',
+        type: 'Perna',
+        description: 'Descrição do exercício'
+    }
+];
