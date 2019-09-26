d657844d2c3f0d21c9595423c2f2ee5c242cc92e
frede122 <fredemanduoliveira@hotmail.com>
2019-09-26 19:37:56 -0300

    router login para training

--- Artefatos/Projeto/client/src/app/app-routing.module.ts
+++ Artefatos/Projeto/client/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { TrainingComponent } from './training/training.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: 'training', component: TrainingComponent},
   { path: '**', component: PageNotFoundComponent },
+
 ];
 
 @NgModule({
