2f8665b7699d3d0fc8626ce0cbc0e4aeb69a2aed
paulobusch <paulo202015@outlook.com.br>
2019-10-03 00:04:45 -0300

    Atualizado script da tabela de usuários

--- Artefatos/Projeto/server/db/workingbyte.sql
+++ Artefatos/Projeto/server/db/workingbyte.sql
@@ -4,11 +4,21 @@ use workingbyte;
 
 -- tabela de usuários
 create table users(
-    id int not null identity(1,1) primary key,
-    name varchar(45) not null,
-    email varhcar(45) not null,
-    endereco varchar(45) not null,
-    idade int not null,
-    login varchar(45) not null,
-    password varchar(45) not null
-);
+    id int not null auto_increment,
+    first_name varchar(30) not null,
+    last_name varchar(60) not null,
+    email varchar(200) not null,
+    address varchar(200) not null,
+    phone varchar(20) not null,
+    cpf varchar(14) not null,
+    age int,
+    is_personal bit not null default 0,
+    sexo char(1) not null,
+    login varchar(50) not null,
+    password varchar(50) not null,
+    removed bit not null default 0,
+    
+    constraint PK_users primary key(id),
+    constraint CK_users check(sexo in('M', 'F')),
+    constraint UQ_users unique(first_name, last_name)
+); 
