d277ea884ecf30cf6519f27d46e825e0ad1cd85e
EnioAmarantes <enio.amarantes@gmail.com>
2019-10-03 20:55:14 -0300

    criação de lista de usuários

--- Artefatos/Projeto/client/src/app/app-routing.module.ts
+++ Artefatos/Projeto/client/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { TrainingComponent } from './training/training.component';
 import { MenuComponent } from './shared/menu/menu.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'menu', component: MenuComponent, children:[
     { path: 'training', component: TrainingComponent},
+    { path: 'user/user-list', component: UserListComponent},
     { path: '',   redirectTo: '/menu/training', pathMatch: 'full' },
+
   ] },
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
