ceae5431dfc4b6fbc5972d8020695b33102ad201
paulobusch <paulo202015@outlook.com.br>
2019-10-04 00:44:10 -0300

    Tratamento de erros

--- Artefatos/Projeto/server/src/controllers/users.controller.js
+++ Artefatos/Projeto/server/src/controllers/users.controller.js
@@ -4,7 +4,8 @@ const { User } = require('../utils/sequelize');
 //Listar todos
 routes.get('/users', async (req, res) => {
     await User.findAll()
-        .then((users) => res.json(users));
+        .then((users) => res.json(users))
+        .catch(error => res.status(500).json(error.errors));
 });
 
 //Buscar pelo id
@@ -12,14 +13,15 @@ routes.get('/users/:id', async (req, res) => {
     const { id } = req.params;
     await User.findByPk(id)
         .then(user => res.json(user))
-        .catch(error => res.status(400));
+        .catch(error => res.status(500).json(error.errors));
 });
 
 //Criar usuário
 routes.post('/users', async (req, res) => {
     console.log(req.body);
     await User.create(req.body)
-        .then(result => res.json(result));
+        .then(result => res.json(result))
+        .catch(error => res.status(500).json(error.errors));
 });
 
 //Atualizar
@@ -28,7 +30,8 @@ routes.put('/users/:id', async (req, res) => {
     await User.udpate(req.body, { 
         where: { id: id }
     })
-    .then(result => res.json(result));
+    .then(result => res.json(result))
+    .catch(error => res.status(500).json(error.errors));
 });
 
 //Remover
@@ -37,7 +40,8 @@ routes.delete('/users/:id', async (req, res) => {
     await User.destroy({
         where: { id: id }
     })
-    .then(result => res.json(result));
+    .then(result => res.json(result))
+    .catch(error => res.status(500).json(error.errors));
 });
 
 module.exports = routes;
