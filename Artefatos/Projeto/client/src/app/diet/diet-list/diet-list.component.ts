c1b93117401782a786d5e1978034529c236ecd25
paulobusch <paulo202015@outlook.com.br>
2019-10-25 22:08:47 -0300

    Corrigido projeto

--- Artefatos/Projeto/client/src/app/diet/diet-list/diet-list.component.ts
+++ Artefatos/Projeto/client/src/app/diet/diet-list/diet-list.component.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diet-list',
+  templateUrl: './diet-list.component.html',
+  styleUrls: ['./diet-list.component.scss']
+})
 export class DietListComponent {
     
   constructor() { }
