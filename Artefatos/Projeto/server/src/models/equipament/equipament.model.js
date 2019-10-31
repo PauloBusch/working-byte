3cffef80e361ac49c37512030b6746105e1e863c
paulobusch <paulo202015@outlook.com.br>
2019-10-30 21:35:49 -0300

    Equipamentos

--- Artefatos/Projeto/server/src/models/equipament/equipament.model.js
+++ Artefatos/Projeto/server/src/models/equipament/equipament.model.js
@@ -4,8 +4,30 @@ const EquipamentModel = (sequelize, dataTypes) => {
     const Equipament = sequelize.define('equipament', {
         id: {
             type: dataTypes.STRING(8),
-            defaultValue: NewId
-        }
+            defaultValue: NewId,
+            primaryKey: true
+        },
+        name: {
+            type: dataTypes.STRING(50),
+            allowNull: false
+        },
+        code: {
+            type: dataTypes.STRING(10),
+            allowNull: false
+        },
+        is_disponible: {
+            type: dataTypes.BOOLEAN,
+            defaultValue: true
+        },
+        removed: {
+            type: dataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        equipament_created: dataTypes.DATE,
+        equipament_updated: dataTypes.DATE
+    },{
+        createdAt: 'equipament_created',
+        updatedAt: 'equipament_updated'
     });
 }
 
---
+++ Artefatos/Projeto/server/src/models/equipament/queries/getEquipamentQuery.js
@@ -0,0 +1,39 @@
+const { Query } = require('../../../utils/interfaces/query');
+ 
+class GetEquipamentQuery extends Query {
+    constructor(
+        id
+    ){
+        super();
+        this.id = id;
+    }
+
+    async GetError(){
+        if (!this.id)
+            return new Error(EErrorCode.InvalidParams, "Parameter id cannot be null");
+
+        const exists = await EquipamentDb.count({ where: { id: this.id, removed: false } });
+        if (!exists)
+            return new Error(EErrorCode.NotFount, `Equipament with id: ${this.id} does not exists`);
+
+        return null;
+    }
+
+    async HasPermission(){
+        return true;
+    }
+
+    async Execute(){
+        const query = { 
+            attributes: ['id','name','code','is_disponible'],
+            where: { id: this.id } 
+        };
+
+        const equipament = await EquipamentDb.findOne(query);
+        return new QueryResult(1, [equipament]);        
+    }
+}
+
+module.exports = {
+    GetEquipamentQuery
+}
