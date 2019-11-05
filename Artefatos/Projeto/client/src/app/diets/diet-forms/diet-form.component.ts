234aa3e783f2d1d1ea975ad7446bd06a450a5895
Enio Amarantes <37565576+EnioAmarantes@users.noreply.github.com>
2019-11-05 20:46:26 -0300

    Add files via upload

--- Artefatos/Projeto/client/src/app/diets/diet-forms/diet-form.component.ts
+++ Artefatos/Projeto/client/src/app/diets/diet-forms/diet-form.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { MatBottomSheet } from '@angular/material';
+import { MatBottomSheet, MatSnackBar } from '@angular/material';
+import { EErrorCode } from 'src/app/shared/models/EErrorCode.model';
+import { GetDietQuery } from '../models/queries/GetDietQuery';
+
+import { DietService } from 'src/app/shared/services/diet.service';
+import { Patterns } from 'src/app/shared/utils/validators';
+import { DietList } from '../models/view-models/diet.list';
+import { DataService } from 'src/app/shared/services/data.service';
+import { Random } from 'src/app/shared/utils/random';
+import { CreateDietCommand } from '../models/commands/createDietCommand';
+import { UpdateDietCommand } from '../models/commands/updateDietCommand';
+
 
 @Component({
   selector: 'app-diet-form',
@@ -16,11 +27,16 @@ export class DietComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private paterns: Patterns,
+    private random: Random,
+    private snackBar: MatSnackBar,
     private bottomSheet: MatBottomSheet,
+    private dietService: DietService,
+    private dataService: DataService<DietList>
   ) { 
     this.form = fb.group({
       id: ['',Validators.required],
-      name: ['', Validators.required],
+      diet_name: ['', Validators.required],
       description: ['', Validators.required],
       id_training: ['', Validators.required],
       id_user: ['', Validators.required],
@@ -28,11 +44,103 @@ export class DietComponent implements OnInit {
     })
   }
 
-  ngOnInit() {
+  ngOnInit(){
+
   }
+
+  loadData(params: { id:string }) {
+  this.refId = params.id;
+  if(!this.refId){
+    this.isNew = true;
+    return;
+  }
+
+  const query = new GetDietQuery(this.refId);
+  this.dietService.getDietById(query).subscribe(result => {
+    if(result.ErrorCode === EErrorCode.NotFound || result.Rows === 0){
+      this.snackBar.open('Dieta não encontrada', 'OK', { duration: 3000 });
+      return;
+    }
+
+    const diet = result.List[0];
+    this.form.patchValue(diet);
+  })
+}
+
 
   close() {
     this.bottomSheet.dismiss();
   }
 
+  save() {
+    if (this.form.invalid) {
+      this.snackBar.open('Preencha os campos obrigatórios', 'OK', { duration: 3000 });
+      return;
+    }
+
+    const value = this.form.value;
+    if (this.isNew) {
+      this.create(value);
+    } else {
+      this.update(value);
+    }
+  }
+
+  private create(values: any) {
+    this.refId = this.random.NewId();
+    const command = new CreateDietCommand(
+      this.refId,
+      values.diet_name,
+      values.description
+    );
+    this.dietService.createDiet(command).subscribe(result => {
+      if (result.ErrorCode ===  EErrorCode.None) {
+        this.snackBar.open('Dieta salva com sucesso', 'OK', { duration: 3000 });
+        this.updateList(values);
+        this.close();
+        return;
+      }
+      if (result.ErrorCode ===  EErrorCode.InvalidParams) {
+        this.snackBar.open('Existem campos inválidos', 'OK', { duration: 3000 });
+        return;
+      }
+
+      this.snackBar.open(result.Message, 'OK', { duration: 3000 });
+    });
+  }
+
+  private update(values: any) {
+    const command = new UpdateDietCommand(
+      this.refId,
+      values.diet_name,
+      values.description
+    );
+    this.dietService.updateDiet(command).subscribe(result => {
+      if (result.ErrorCode ===  EErrorCode.None) {
+        this.snackBar.open('Dieta editada com sucesso', 'OK', { duration: 3000 });
+        this.updateList(values);
+        this.close();
+        return;
+      }
+      if (result.ErrorCode ===  EErrorCode.InvalidParams) {
+        this.snackBar.open('Existem campos inválidos', 'OK', { duration: 3000 });
+        return;
+      }
+     
+      this.snackBar.open(result.Message, 'OK', { duration: 3000 });
+    });
+  }
+
+  private updateList(values: any) {
+    const diet = new DietList(
+      this.refId,
+      values.diet_name,
+      values.description
+    );
+    this.dataService.update(diet);
+  }
+
+  hasError(field: string, error: string): boolean {
+    return this.form.get(field).hasError(error);
+  }
 }
