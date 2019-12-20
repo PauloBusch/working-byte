6a83eb10ca1778b0992e0ec977257a1117ac3f4e
frede122 <fredemanduoliveira@hotmail.com>
2019-12-19 23:18:49 -0300

    Att Exercise

--- Artefatos/Projeto/client/src/app/training/exercise-form/exercise-form.component.ts
+++ Artefatos/Projeto/client/src/app/training/exercise-form/exercise-form.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
 import { TrainingListComponent } from '../training-list/training-list.component';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import {coerceNumberProperty} from '@angular/cdk/coercion';
 import { TrainingExerciseDetails } from '../models/view-models/trainingExercise.details';
 import { tick } from '@angular/core/testing';
+import { startWith, map } from 'rxjs/operators';
+import { EquipmentList } from 'src/app/equipments/models/view-models/equipment.list';
+import { Observable } from 'rxjs';
+import { EquipmentService } from 'src/app/shared/services/equipment.service';
+import { ListEquipmentQuery } from 'src/app/equipments/models/queries/listEquipmentQuery';
+import { Storage } from 'src/app/shared/utils/storage';
 
 
 @Component({
@@ -15,8 +21,11 @@ import { tick } from '@angular/core/testing';
 export class ExerciseFormComponent implements OnInit {
   private form: FormGroup;
   private isNew: boolean;
+  options: EquipmentList[];
+  filteredOptions: Observable<EquipmentList[]>;
 
   public exercise = new TrainingExerciseDetails(null, "", "", "", "", 0, 0, 0 );
+  listQuery: ListEquipmentQuery;
    formatLabel(value: number) {
     if (value >= 10) {
       return Math.round(value / 10) + 'k';
@@ -26,10 +35,11 @@ export class ExerciseFormComponent implements OnInit {
   }
 
 
-
   constructor(
       private fb: FormBuilder,
       public dialogRef: MatDialogRef<any>,
+      private equipService: EquipmentService,
+      private snackBar: MatSnackBar,
       
       @Inject(MAT_DIALOG_DATA) public data: TrainingExerciseDetails
     ) {
@@ -40,24 +50,53 @@ export class ExerciseFormComponent implements OnInit {
         equipment: ['', Validators.required]
 
       });
+      const limit = Storage.get('equipments.limit', 10);
+      const page = Storage.get('equipments.page', 1);
+      const sortAsc = Storage.get('equipments.sortAsc', false);
+      const columnSort = Storage.get('equipments.columnSort', 'equipment_created');
+      this.listQuery = new ListEquipmentQuery(page, limit, sortAsc, columnSort);
 
       this.loadData();
     }
 
-    loadData() {
-      
-      if(!this.data){
-        this.isNew = true;
-        return;
-      }
-      
-      this.exercise = this.data;
 
+  loadData() {
+    
+    if(!this.data){
+      this.isNew = true;
+      return;
     }
+    
+    this.exercise = this.data;
+
+  }
+
+  loadEquip(){
+
+      this.equipService.getEquipments(this.listQuery).subscribe(result => {
+      this.options = result.List;
+      this.filterEquip();
+      for(let op of this.options){
+        if(op.id == this.exercise.id_equipment)
+          this.form.controls.equipment.setValue(op.name);
+      }
+    });
+
+  }
 
   close(): void {
     this.exercise = null;
     this.dialogRef.close();
+  }
+
+  save(): void {
+    if (this.form.invalid) {
+      this.snackBar.open('Preencha os campos obrigatórios', 'OK', { duration: 3000 });
+      return;
+    }
+
+    this.dialogRef.close();
+
   }
 
   hasError(field: string, error: string): boolean {
@@ -65,6 +104,21 @@ export class ExerciseFormComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.loadEquip();
+  }
+
+  private _filter(name: string): EquipmentList[] {
+    const filterValue = name.toString().toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+  filterEquip(){
+    this.filteredOptions = this.form.controls.equipment.valueChanges
+    .pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
   }
 
 }
